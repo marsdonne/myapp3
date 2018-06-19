c88748a1085105df5f3e04caeaccd0a874b1e084
marsdonne@live.com <marsdonne@live.com>
2018-06-19 14:38:28 +0800

    #DEMO2-1 t1 #DONE

--- grails-app/domain/myapp2/MyDomain.groovy
+++ grails-app/domain/myapp2/MyDomain.groovy
@@ -7,10 +7,18 @@ class MyDomain {
     Integer gender
     String addr
 
+    Integer age
+
+    static transients = ['age']
+
     static constraints = {
         name(blank: false)
         birthday(nullable: false, validator: { it < new Date() })
         gender(nullable: false, range: 0..1)
         addr(blank: false)
+    }
+
+    def afterLoad() {
+        age = (new Date() - birthday) / 365 as int
     }
 }
